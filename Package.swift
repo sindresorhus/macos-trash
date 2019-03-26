f85ae972e0a041e5f9a461a79db9265e8535234d
Sindre Sorhus <sindresorhus@gmail.com>
2019-03-26 15:40:31 +0700

    Upgrade to Swift 5

--- Package.swift
+++ Package.swift
@@ -1,8 +1,11 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
 	name: "trash",
+	platforms: [
+		.macOS(.v10_9)
+	],
 	targets: [
 		.target(
 			name: "trash"
