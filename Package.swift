4fd6ca9077be672ffd7d854111fc1feb24ca2d44
Sindre Sorhus <sindresorhus@gmail.com>
2021-01-07 00:40:53 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
@@ -7,11 +7,9 @@ let package = Package(
 		.macOS(.v10_10)
 	],
 	products: [
-		.executable(name: "trash", targets: ["trash"]),
+		.executable(name: "trash", targets: ["trash"])
 	],
 	targets: [
-		.target(
-			name: "trash"
-		)
+		.target(name: "trash")
 	]
 )
