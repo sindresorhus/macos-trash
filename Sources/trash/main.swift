523b330716e15cd793f45653578d0fc7089aaeaa
Yuhki Yano <meijo-hakken.r15@kvd.biglobe.ne.jp>
2025-03-09 08:22:50 +0900

    Fix #23: Modify the process for interactive

--- Sources/trash/main.swift
+++ Sources/trash/main.swift
@@ -26,6 +26,18 @@ func prompt(question: String) -> Bool {
 	return ["y", "yes"].contains(input.lowercased())
 }
 
+func secondarg(arg: String) -> Void {
+	if ["--help", "-h"].contains(arg) {
+	    print("Usage: trash [--help | -h] [--version | -v] [--interactive | -i] <path> […]")
+	    exit(0)
+	}
+	
+	if ["--version", "-v"].contains(arg) {
+	    print(VERSION)
+	    exit(0)
+	}
+}
+
 guard let argument = CLI.arguments.first else {
 	print("Specify one or more paths", to: .standardError)
 	exit(1)
@@ -40,16 +52,20 @@ case "--version", "-v":
 	exit(0)
 case "--interactive", "-i":
 	for url in (CLI.arguments.dropFirst().map { URL(fileURLWithPath: $0) }) {
-		guard FileManager.default.fileExists(atPath: url.path) else {
-			print("The file “\(url.relativePath)” doesn't exist.")
-			continue
-		}
-
-		guard prompt(question: "Trash “\(url.relativePath)”?") else {
-			continue
-		}
-
-		trash([url])
+	    if ["--help", "-h", "--version", "-v"].contains(url.relativePath) {
+	        secondarg(arg: url.relativePath)
+	    }
+	    
+	    guard FileManager.default.fileExists(atPath: url.path) else {
+	    	print("The file “\(url.relativePath)” doesn't exist.")
+	    	continue
+	    }
+	    
+	    guard prompt(question: "Trash “\(url.relativePath)”?") else {
+	    	continue
+	    }
+	    
+	    trash([url])
 	}
 default:
 	trash(CLI.arguments.map { URL(fileURLWithPath: $0) })
