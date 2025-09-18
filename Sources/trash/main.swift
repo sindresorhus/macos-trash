0e3eb06126d245dd5a7b4698fc54dd6b0b373649
Lucas Garron <code@garron.net>
2025-09-17 19:36:47 -0700

    Add `--` semantics for other flags that take a list of paths (`--interactive` / `-i`).

--- Sources/trash/main.swift
+++ Sources/trash/main.swift
@@ -31,6 +31,15 @@ guard let argument = CLI.arguments.first else {
 	exit(1)
 }
 
+// Handle positionals, at the point when no other flags will be accepted.
+// If there is a leading `--` argument, it will be removed (but not any subsequent `--` arguments).
+func collectPaths(args: ArraySlice<String>) -> ArraySlice<String> {
+	if args.count > 0 && args[args.startIndex] == "--" {
+		return args.dropFirst()
+	}
+	return args
+}
+
 switch argument {
 case "--help", "-h":
 	print("Usage: trash [--help | -h] [--version | -v] [--interactive | -i] <path> […]")
@@ -39,7 +48,7 @@ case "--version", "-v":
 	print(VERSION)
 	exit(0)
 case "--interactive", "-i":
-	for url in (CLI.arguments.dropFirst().map { URL(fileURLWithPath: $0) }) {
+	for url in (collectPaths(args: CLI.arguments.dropFirst()).map { URL(fileURLWithPath: $0) }) {
 		guard FileManager.default.fileExists(atPath: url.path) else {
 			print("The file “\(url.relativePath)” doesn't exist.")
 			continue
@@ -51,8 +60,6 @@ case "--interactive", "-i":
 
 		trash([url])
 	}
-case "--":
-	trash(CLI.arguments.dropFirst().map { URL(fileURLWithPath: $0) })
 default:
-	trash(CLI.arguments.map { URL(fileURLWithPath: $0) })
+	trash(collectPaths(args: ArraySlice(CLI.arguments)).map { URL(fileURLWithPath: $0) })
 }
